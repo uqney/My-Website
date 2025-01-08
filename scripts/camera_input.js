fc4933fd0d37529b0df2408754a8dcc38283f96c
uqney <uqney@student.kit.edu>
2025-01-08 17:52:52 +0100

    Try something with camera input on mobile devices.

--- scripts/camera_input.js
+++ scripts/camera_input.js
@@ -4,22 +4,21 @@ const closeCameraButton = document.getElementById('close-camera');
 const cameraContainer = document.querySelector('.camera-container');
 const captureButton = document.getElementById('capture-button');
 const captureCanvas = document.getElementById('capture-canvas');
+const cameraInput = document.getElementById('camera-input');
 
 // Initially hide the camera container
 cameraContainer.style.display = 'none';
 
 activateCameraButton.addEventListener('click', () => {
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', width: { ideal: 3840 } }, height: { ideal: 2160 } })
-        .then((stream) => {
-            cameraContainer.style.display = 'block'; // Show the camera container
-            const video = document.getElementById('camera-preview');
-            video.srcObject = stream;
-        })
-        .catch((error) => {
-            console.error('Error accessing the camera:', error);
-            alert('Unable to access the camera. Please check your permissions.');
-        });
+    cameraInput.click();
 });
+
+cameraInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    const fileName = file.name;
+    const baseName = fileName.substring(0, fileName.lastIndexOf('.'));
+    uploadFromFile(file, baseName, uploadInput);
+})
 
 // Capture image logic
 captureButton.addEventListener('click', () => {
